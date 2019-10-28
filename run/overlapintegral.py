91705cc6317e98f998d749b4685f0948a27384e3
Kevin <kevin.jablonka@epfl.ch>
2019-10-28 14:31:53 +0100

    Adding print statements

--- run/overlapintegral.py
+++ run/overlapintegral.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from __future__ import print_function
 import click
 from cube import Cube
 
@@ -8,10 +9,12 @@ from cube import Cube
 @click.argument('path0', type=click.Path(exists=True))
 @click.argument('path1', type=click.Path(exists=True))
 def main(path0, path1):
+    print('*** Reading cubes ****')
     cube0 = Cube(path0)
     cube1 = Cube(path1)
 
-    return cube0 * cube1
+    print('*** Calculating overlap ***')
+    print((cube0 * cube1))
 
 
 if __name__ == '__main__':
