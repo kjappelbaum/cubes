2790647306318dbbdf8878f154f6f37169178e65
Kevin <kevin.jablonka@epfl.ch>
2019-10-28 12:03:04 +0100

    added runscript

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
+with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="cube",
-    version="0.1dev",
+    name='cube',
+    version='0.1dev',
     packages=find_packages(),
-    license="MIT",
+    license='MIT',
+    entry_points={'console_scripts': ['overlapintegral=run.overlapintegral:main']},
     install_requires=requirements,
     extras_require={
-        "develop": ["pytest", "pre-commit", "black", "prospector", "pylint"]
+        'develop': ['pytest', 'pre-commit', 'black', 'prospector', 'pylint']
     },
-    long_description=open("README.md").read(),
+    long_description=open('README.md').read(),
 )
